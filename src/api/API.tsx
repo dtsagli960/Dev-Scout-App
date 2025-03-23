de50afde0f1cae65a5452a4ba4b6a182508fb1a8
Daniel Tsagli <Danieltsagli960@gmail.com>
2025-03-23 02:16:03 +0000

    Update API.tsx

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -6,7 +6,7 @@ const searchGithub = async () => {
       `https://api.github.com/users?since=${start}`,
       {
         headers: {
-          Authorization: `Bearer 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`,
+          Authorization: `Bearer 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`,
         },
       }
     );
@@ -27,7 +27,7 @@ const searchGithubUser = async (username: string) => {
   try {
     const response = await fetch(`https://api.github.com/users/${username}`, {
       headers: {
-        Authorization: `Bearer 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`,
+        Authorization: `Bearer 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`,
       },
     });
     const data = await response.json();
